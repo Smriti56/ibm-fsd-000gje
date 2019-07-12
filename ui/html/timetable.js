dc6bc251b2e4e0ab4c75fa0d79c1abb843ed2df0
SMRITI CHAURASIA <smchaur1@in.ibm.com>
2019-07-12 13:45:38 +0530

    Timetable Assessment

--- ui/html/timetable.js
+++ ui/html/timetable.js
@@ -19,52 +19,112 @@ function assign()
     {
         if(cls=='A')
         {
+            if(localStorage.getItem("att1")==undefined)
+            {
             document.getElementById('att1').value=teacher;
             document.getElementById('ats1').value=sub;
+            localStorage.setItem("att1","filled");
+            }
+            else
+            {
+                alert("Already filled");
+            }
         }
         else if(cls=='B')
         {
-            document.getElementById('btt1').value=teacher;
-            document.getElementById('bts1').value=sub;
+            if(localStorage.getItem("btt1")==undefined)
+            {
+                document.getElementById('btt1').value=teacher;
+                document.getElementById('bts1').value=sub;
+                localStorage.setItem("btt1","filled");
+            }
+            else
+            {
+                alert("Already filled");
+            }
         }
     }
     else if(tm=='t2')
     {
         if(cls=='A')
         {
-            document.getElementById('att2').value=teacher;
-            document.getElementById('ats2').value=sub;
+            if(localStorage.getItem("att2")==undefined){
+                document.getElementById('att2').value=teacher;
+                document.getElementById('ats2').value=sub;
+                localStorage.setItem("att2","filled");
+
+            }
+            else
+            {
+                alert("Already filled");
+            }
+           
         }
         else if(cls=='B')
         {
+            if(localStorage.getItem("btt2")==undefined){
             document.getElementById('btt2').value=teacher;
             document.getElementById('bts2').value=sub;
+            localStorage.setItem("btt2","filled");
+            }
+            else
+            {
+                alert("Already filled");
+            }
         }
     }
     else if(tm=='t3')
     {
         if(cls=='A')
         {
+            if(localStorage.getItem("att3")==undefined){
             document.getElementById('att3').value=teacher;
             document.getElementById('ats3').value=sub;
+            localStorage.setItem("att3","filled");
+            }
+            else
+            {
+                alert("Already filled");
+            }
         }
         else if(cls=='B')
         {
+            if(localStorage.getItem("btt3")==undefined){
             document.getElementById('btt3').value=teacher;
             document.getElementById('bts3').value=sub;
+            localStorage.setItem("btt3","filled");
+            }
+            else
+            {
+                alert("Already filled");
+            }
         }
     }
     else if (tm=='t4')
     {
         if(cls=='A')
         {
+            if(localStorage.getItem("att4")==undefined){
             document.getElementById('att4').value=teacher;
             document.getElementById('ats4').value=sub;
+            localStorage.setItem("att4","filled");
+            }
+            else
+            {
+                alert("Already filled");
+            }
         }
         else if(cls=='B')
         {
+            if(localStorage.getItem("btt4")==undefined){
             document.getElementById('btt4').value=teacher;
             document.getElementById('bts4').value=sub;
+            localStorage.setItem("btt4","filled");
+            }
+            else
+            {
+                alert("Already filled");
+            }
         }
     }
     // let subs= document.getElementsByName("sub");
