25b64a9ad62337017c6ea717d39e6882f9f47055
b4ibmjava17@iiht.tech <b4ibmjava17@iiht.tech>
2019-09-10 14:39:23 +0530

    spring-rest-complete

--- javaSpring/spring-rest-crm/src/main/java/comm/example/spring/rest/controller/CustomerRestController.java
+++ javaSpring/spring-rest-crm/src/main/java/comm/example/spring/rest/controller/CustomerRestController.java
@@ -2,8 +2,14 @@ package comm.example.spring.rest.controller;
 
 import java.util.List;
 
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +29,30 @@ public class CustomerRestController {
 		return service.getCustomers();
 		
 	}
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer customer) {
+	
+		customer.setId(0);
+		service.createCustomer(customer);
+		return customer;
+	}
+	
+	@DeleteMapping("/customers/{id}") 
+	public Customer deleteCustomer(@PathVariable int id) {
+		Customer customer=service.getCustomerById(id);
+		service.deleteCustomer(customer);
+		return customer;
+	}
+	
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer)
+	{
+		service.editCustomer(customer);
+		return customer;
+	}
+	
+	
 
 }
