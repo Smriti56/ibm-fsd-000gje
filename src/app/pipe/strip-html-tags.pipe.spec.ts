6609acadcf57f9ed01b168f2bbd5c3cb2acc780a
b4ibmjava17@iiht.tech <b4ibmjava17@iiht.tech>
2019-09-30 15:12:35 +0530

    updated project

--- src/app/pipe/strip-html-tags.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StripHtmlTagsPipe } from './strip-html-tags.pipe';
-
-describe('StripHtmlTagsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StripHtmlTagsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
